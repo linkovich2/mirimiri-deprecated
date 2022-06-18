8137f85b5f17fc83a65d1704a31c682a4ffa24d4
link <kaos78414@yahoo.com>
2022-06-17 21:15:55 -0500

    Add a filter, no need for pagination for now

--- src/components/dev/Locations.vue
+++ src/components/dev/Locations.vue
@@ -2,10 +2,12 @@
   <div>
     <h1>Locations</h1>
 
+    <input type="text" v-model="search" placeholder="Filter Locations" />
+
     <ul>
-      <li v-for="location in locations" :key="location">
-        {{ location.replace('.json', '') }}
-        <span class="delete" @click="deleteLocation(location)">x</span>
+      <li v-for="(location, index) in filteredLocations.slice(0, limit)" :key="location">
+        <strong>{{ location.replace('.json', '') }}</strong>
+        <span class="delete" @click="deleteLocation(location, index)">x</span>
       </li>
     </ul>
 
@@ -29,10 +31,13 @@ export default {
     return {
       locations: [],
       resource: 'locations',
+      page: 1,
       newLocation: {
         name: "",
         id: ""
-      }
+      },
+      search: "",
+      limit: 15
     }
   },
   beforeMount() {
@@ -58,11 +63,20 @@ export default {
         this.newLocation = { id: "", name: "" }
       })
     },
-    deleteLocation(location) {
-      let filePath = `${this.resource}/${location.replace('.json', '')}`
-      ipcRenderer.invoke('dev-delete-content-file', { filePath: filePath }).then(() => {
-        this.locations.splice(this.locations.indexOf(location), 1)
-      })
+    deleteLocation(location, index) {
+      if (confirm("You sure? A lot of work went into this.")) {
+        let filePath = `${this.resource}/${location.replace('.json', '')}`
+        ipcRenderer.invoke('dev-delete-content-file', { filePath: filePath }).then(() => {
+          this.locations.splice(index, 1)
+        })
+      }
+    }
+  },
+  computed: {
+    filteredLocations() {
+     return this.locations.filter(location => {
+        return location.toLowerCase().includes(this.search.toLowerCase())
+     })
     }
   }
 }
@@ -98,8 +112,13 @@ export default {
     margin: 20px;
   }
 
+  strong {
+    font-size: 1.2rem;
+  }
+
   .delete {
     color: red;
     cursor: pointer;
+    margin-left: 20px;
   }
 </style>

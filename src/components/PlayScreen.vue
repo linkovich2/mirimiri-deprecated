a6b21a983eef85df6628482d95e624f2c4edfe2e
link <kaos78414@yahoo.com>
2022-06-17 22:37:31 -0500

    Add pixi and camera controls

--- src/components/PlayScreen.vue
+++ src/components/PlayScreen.vue
@@ -1,17 +1,57 @@
 <template>
   <div id="container">
-    <div class="debug-menu">Debug: <router-link to="/travel">Travel</router-link></div>
-    <p>Playing as: {{ game.character.name }}</p>
+    <div class="debug-menu">Playing as: {{ game.character.name }} | Debug: <router-link to="/travel">Travel</router-link></div>
     <div class="settings-cog" @contextmenu="settingsContext($event)" @click="settingsContext($event)">&#9881;</div>
   </div>
 </template>
 
 <script>
+import * as PIXI from 'pixi.js'
+import { Viewport  } from 'pixi-viewport'
 const { ipcRenderer } = require('electron')
 
 export default {
   name: 'PlayScreen',
   inject: ['game'],
+  data() {
+    return {
+      pixiApp: null,
+      canvas: null
+    }
+  },
+  beforeMount() {
+    this.pixiApp = new PIXI.Application()
+
+    const viewport = new Viewport({
+        passiveWheel: false,
+        worldWidth: 1000,
+        worldHeight: 1000,
+
+        interaction: this.pixiApp.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    })
+
+    this.pixiApp.renderer.backgroundColor = 0xFEFEFE
+    this.pixiApp.stage.addChild(viewport)
+    viewport
+        .drag()
+        .pinch()
+        .wheel()
+        .decelerate()
+
+    this.canvas = document.body.appendChild(this.pixiApp.view)
+
+    const sprite = viewport.addChild(new PIXI.Sprite(PIXI.Texture.WHITE))
+
+    sprite.tint = 0xff0000
+    sprite.width = sprite.height = 100
+    sprite.position.set(100, 100)
+  },
+  unmounted() {
+    this.pixiApp.stage.destroy(true)
+    this.pixiApp.stage = null
+    this.pixiApp = null
+    this.canvas.remove()
+  },
   methods: {
     close() {
       ipcRenderer.invoke('quit')
@@ -56,6 +96,10 @@ export default {
     bottom: 0;
     margin: 20px;
     right: 0;
+  }
+
+  #container * {
+    z-index: 2;
   }
 
   .debug-menu {
